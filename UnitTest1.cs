e793a626e92797703b3af55c95466f22cf1fbce2
mgrigorevska <111455074+mgrigorevska@users.noreply.github.com>
2024-04-14 22:06:40 +0500

    added comments in UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,24 +9,30 @@ namespace Selenium_test_Grigorevska
         [Test]
         public void Authorization()
         {
+            // задаем параметры
             var options = new ChromeOptions();
             options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
             
+            // перейти по url https://staff-testing.testkontur.ru/
             var driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
             Thread.Sleep(1000);
 
+            // ввести логин
             var login = driver.FindElement(By.Id("Username"));
             login.SendKeys("mgliezz@gmail.com");
 
+            // ввести пароль
             var password = driver.FindElement(By.Name("Password"));
             password.SendKeys("h0wXyTE5!1");
             Thread.Sleep(1000);
 
+            // нажать кнопку Войти
             var button = driver.FindElement(By.Name("button"));
             button.Click();
             Thread.Sleep(3000);
-
+            
+            // проверить соответствие значений нового урл
             var current_url = driver.Url;
             Assert.That(current_url == "https://staff-testing.testkontur.ru/news");
             
