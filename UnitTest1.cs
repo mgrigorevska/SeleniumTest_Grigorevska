69d655092b918e8f917a8d6d0a3a468267354ac9
mgrigorevska <111455074+mgrigorevska@users.noreply.github.com>
2024-04-22 14:22:41 +0500

    created AutoTestHomework

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,6 +1,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 
+
 namespace Selenium_test_Grigorevska
 {
     public class Selenium_test
@@ -11,26 +12,27 @@ namespace Selenium_test_Grigorevska
             // задаем параметры
             var options = new ChromeOptions();
             options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
-            
+                        
             // перейти по url https://staff-testing.testkontur.ru/
             var driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
             Thread.Sleep(1000);
-
+            
             // ввести логин
-            var login = driver.FindElement(By.Id("Username"));
+            var login = driver.FindElement(By.Id("Username")); 
+            
             login.SendKeys("mgliezz@gmail.com");
-
+            
             // ввести пароль
             var password = driver.FindElement(By.Name("Password"));
             password.SendKeys("***");
             Thread.Sleep(1000);
-
+            
             // нажать кнопку Войти
             var button = driver.FindElement(By.Name("button"));
             button.Click();
             Thread.Sleep(3000);
-            
+                        
             // проверить соответствие значений нового урл
             var current_url = driver.Url;
             Assert.That(current_url == "https://staff-testing.testkontur.ru/news");
